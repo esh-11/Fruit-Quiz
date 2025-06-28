4aac468c40f8e3b316f30843a663fe91eec32816
Eshani Ranasinghe <eshanisr@gmail.com>
2025-06-28 12:38:13 -0700

    Add files via upload

---
+++ fruit_quiz.py
@@ -0,0 +1,48 @@
+# Name:  
+# Student Number:  
+
+# This file is provided to you as a starting point for the "fruit_quiz.py" program of the Project
+# of Programming Principles in Summer School, 2025.  It aims to give you just enough code to help ensure
+# that your program is well structured.  Please use this file as the basis of your work.
+# You are not required to reference it.
+
+# The "pass" command tells Python to do nothing.  It is simply a placeholder to ensure that the starter file runs smoothly.
+# They are not needed in your completed program.  Replace them with your own code as you complete the assignment.
+
+
+# Import the necessary module(s).
+import tkinter, tkinter.messagebox, random, json
+
+
+
+class ProgramGUI:
+
+    def __init__(self):
+        # This is the constructor of the class.
+        # It is responsible for loading the data from the text file and creating the user interface.
+        # See the "Constructor of the GUI Class of fruit_quiz.py" section of the assignment brief.  
+        pass
+
+
+
+    def show_question(self):
+        # This method randomly selects two fruit and a nutritional component and displays them in the GUI.
+        # See Point 1 of the "Methods in the GUI class of fruit_quiz.py" section of the assignment brief.
+        pass
+
+
+
+    def check_answer(self, choice):   
+        # This method is responsible for determining whether the user clicked the correct button.
+        # See Point 2 of the "Methods in the GUI class of fruit_quiz.py" section of the assignment brief.
+        pass
+
+
+
+# Create an object of the ProgramGUI class to begin the program.
+gui = ProgramGUI()
+
+
+
+
+# If you have been paid to write this program, please delete this comment.
